904a3dfc8daacdd520c0a3e28fb23325cf10ba2e
Shikanime <shikanime@hotmail.fr>
2017-03-20 15:15:02 +0100

    modified:   src/my_printf.c

--- src/my_printf.c
+++ src/my_printf.c
@@ -1,4 +1,5 @@
 #include "headers/loader.h"
+#define PROCESS_NUMBER 7
 
 // /!\ DEV /!\ NEED TO BE SEPARATE
 size_t		str_length(string s)
@@ -15,7 +16,7 @@ void	chr_print(const char c)
   write(1, &c, 1);
 }
 
-void		str_print(const string s)
+bool		str_print(const string s)
 {
   string	bfs;
 
@@ -54,44 +55,137 @@ void	nbr_print(int n)
 
 // FUNCTIONNAL CODE
 
-void        my_printf(const string * query, ...) {
-    int     i;
+/* Internal prototype */
+
+bool integerCheck();
+bool longCheck();
+bool charCheck();
+bool stringCheck();
+bool pointerCheck();
+bool shortCheck();
+bool hexadecimalCheck();
+
+bool (*check[])(const string query, int i) = {
+   integerCheck,
+   longCheck,
+   charCheck,
+   stringCheck,
+   pointerCheck,
+   shortCheck,
+   hexadecimalCheck
+} ;
+
+void integerPrint();
+void longPrint();
+void charPrint();
+void stringPrint();
+void pointerPrint();
+void shortPrint();
+void hexadecimalPrint();
+
+void (*print[])(var_list ap, char* opt) = {
+   integerPrint,
+   longPrint,
+   charPrint,
+   stringPrint,
+   pointerPrint,
+   shortPrint,
+   hexadecimalPrint
+} ;
+
+/* Usable functions */
+
+void        my_printf(const string query, ...) {
+    int     i,
+            j;
     size_t  querySize;
     va_list ap;
 
     querySize = str_length(query);
 
-    va_start(ap, texte);
+    va_start(ap, query);
 
     i = 0;
     while (i < querySize) {
         if (query[i] == '%') {
-            i++;
-            // Integer
-            if (query[i] == 'd') {
-                nbr_print(var_arg(ap, int));
-            }
+          ++i;
+          j = 0;
+          while(j < PROCESS_NOMBER) {
+             if((*check[j])(query, j))
+                (*print[j])(ap, opt);
+             ++j;
+          }
         } else {
             chr_print(query[i]);
         }
+        ++i;
     }
+
     var_end(ap);
 }
 
-/*
-void my_printf(string str, int intArgs, float fltArgs, const string strArgs, ...) {
-    va_list ap;
-    string  array[MAXARGS + 1];
-    int     argno;
+/* Local functions */
 
-    argno = 0;
-
-    va_start(ap, args);
-    while(argds != 0 && argno < MAXARGS) {
-        array[argno++] = args;
-        args = va_arg(ap, const string);
-    }
-    array[argno] = (string) 0;
-    va_end(ap);
+// Checker
+bool integerCheck(const string query, int n){
+  if (query[n] == 'd')
+    return true;
 }
-*/
+
+bool longCheck(const string query, int n){
+  if (query[n] == 'l' && query[n + 1] == 'd')
+    return true;
+}
+
+bool charCheck(const string query, int n){
+  if (query[n] == 'c' || (query[n]))
+    return true;
+}
+
+bool stringCheck(const string query, int n){
+  if (query[n] == 's')
+    return true;
+}
+
+bool pointerCheck(const string query, int n){
+  if (query[n] == 'p')
+    return true;
+}
+
+bool shortCheck(const string query, int n){
+  if (query[n] == 'h' && query[n + 1] == 'd')
+    return true;
+}
+
+bool hexadecimalCheck(const string query, int n){
+  if (query[n] == 'x')
+    return true;
+}
+
+
+// Printer
+void integerPrint(var_list ap, char* opt){
+  nbr_print(var_arg(ap, int));
+}
+
+void integerPrint(var_list ap, char* opt){
+}
+
+void charPrint(var_list ap, char* opt){
+}
+
+void stringPrint(var_list ap, char* opt){
+
+}
+
+void pointerPrint(var_list ap, char* opt){
+
+}
+
+void shortPrint(var_list ap, char* opt){
+
+}
+
+void hexadecimalPrint(var_list ap, char* opt){
+
+}
