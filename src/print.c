72f0743ebf31179d0926eed9a725451c6473e41d
Shikanime <shikanime@hotmail.fr>
2017-03-21 16:14:31 +0100

    modified:   src/headers/rules.print.h 	modified:   src/print.c

--- src/print.c
+++ src/print.c
@@ -2,7 +2,7 @@
 
 /* Usable functions */
 
-void (*print[7])(va_list * ap, int * opt) = {
+void (*print[7])(va_list * ap, char * query, int * opt) = {
    integerPrint,
    longPrint,
    stringPrint,
@@ -14,30 +14,30 @@ void (*print[7])(va_list * ap, int * opt) = {
 
 /* Local functions */
 
-void integerPrint(va_list * ap, int * opt){
+void integerPrint(va_list * ap, char * query, int * opt){
     va_start(ap, query);
-  stringPrintHelper("alors la?");
-  integerPrintHelper(var_arg(ap, int));
-  stringPrintHelper("et la?");
+    stringPrintHelper("alors la?");
+    integerPrintHelper(var_arg(ap, int));
+    stringPrintHelper("et la?");
     va_end(ap);
 }
 
-void longPrint(va_list * ap, int * opt){
+void longPrint(va_list * ap, char * query, int * opt){
 }
 
-void charPrint(va_list * ap, int * opt){
+void charPrint(va_list * ap, char * query, int * opt){
   stringPrintHelper(var_arg(ap, string));
 }
 
-void stringPrint(va_list * ap, int * opt){
+void stringPrint(va_list * ap, char * query, int * opt){
   stringPrintHelper(var_arg(ap, string));
 }
 
-void pointerPrint(va_list * ap, int * opt){
+void pointerPrint(va_list * ap, char * query, int * opt){
 }
 
-void shortPrint(va_list * ap, int * opt){
+void shortPrint(va_list * ap, char * query, int * opt){
 }
 
-void hexadecimalPrint(va_list * ap, int * opt){
+void hexadecimalPrint(va_list * ap, char * query, int * opt){
 }
