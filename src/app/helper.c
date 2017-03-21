30aee25d16be2cc5abdfbb390642c51eb30d9029
Shikanime <shikanime@hotmail.fr>
2017-03-21 10:11:03 +0100

    modified:   src/app/helper.c 	new file:   src/app/helper.h

--- src/app/helper.c
+++ src/app/helper.c
@@ -1,4 +1,5 @@
 #include "../main.h"
+#include "helper.h"
 
 /* Usable functions */
 
---
+++ src/app/helper.h
@@ -0,0 +1,4 @@
+size_t  stringLengthHelper(string s);
+void	charPrintHelper(const char c);
+void    stringPrintHelper(const string s);
+void    integerPrintHelper(int n);
