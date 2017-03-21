9301707410712ce9521c05e9d3149a6e17db2dc1
Shikanime <shikanime@hotmail.fr>
2017-03-21 13:27:09 +0100

    modified:   src/headers/rules.check.h 	modified:   src/headers/rules.print.h 	modified:   src/loader.h

--- src/headers/rules.print.h
+++ src/headers/rules.print.h
@@ -1,15 +1,15 @@
 #ifndef MY_PRINTF_RULLES_PRINT_H
 #define MY_PRINTF_RULLES_PRINT_H
 
-extern void integerPrint       (va_list ap, char* opt);
-extern void longPrint          (va_list ap, char* opt);
-extern void charPrint          (va_list ap, char* opt);
-extern void stringPrint        (va_list ap, char* opt);
-extern void pointerPrint       (va_list ap, char* opt);
-extern void shortPrint         (va_list ap, char* opt);
-extern void hexadecimalPrint   (va_list ap, char* opt);
+void integerPrint       (va_list ap, char* opt);
+void longPrint          (va_list ap, char* opt);
+void charPrint          (va_list ap, char* opt);
+void stringPrint        (va_list ap, char* opt);
+void pointerPrint       (va_list ap, char* opt);
+void shortPrint         (va_list ap, char* opt);
+void hexadecimalPrint   (va_list ap, char* opt);
 
-extern void (*print[])(va_list ap, char* opt) = {
+void (*print[])(va_list ap, char* opt) = {
    integerPrint,
    longPrint,
    charPrint,
--- src/loader.h
+++ src/loader.h
@@ -23,9 +23,12 @@ typedef char* string;
 
 /* PROJECT FILES */
 
+// Rules
 #include "headers/rules.check.h"
 #include "headers/rules.option.h"
 #include "headers/rules.print.h"
+
+// 
 #include "headers/app.my_printf.h"
 #include "headers/app.helper.h"
 
