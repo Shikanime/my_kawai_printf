81380cb5de5cd73c19cef3e4703018a2316619f3
Shikanime <shikanime@hotmail.fr>
2017-03-21 09:11:43 +0100

    modified:   src/headers/rules.check.h 	modified:   src/headers/rules.print.h

--- src/headers/rules.check.h
+++ src/headers/rules.check.h
@@ -1,13 +1,13 @@
 #ifndef MY_PRINTF_RULLES_CHECK_H
 #define MY_PRINTF_RULLES_CHECK_H
 
-bool integerCheck       (void);
-bool longCheck          (void);
-bool charCheck          (void);
-bool stringCheck        (void);
-bool pointerCheck       (void);
-bool shortCheck         (void);
-bool hexadecimalCheck   (void);
+bool integerCheck       (const string query, int n);
+bool longCheck          (const string query, int n);
+bool charCheck          (const string query, int n);
+bool stringCheck        (const string query, int n);
+bool pointerCheck       (const string query, int n);
+bool shortCheck         (const string query, int n);
+bool hexadecimalCheck   (const string query, int n);
 
 bool (*check[])(const string query, int i) = {
    integerCheck,
--- src/headers/rules.print.h
+++ src/headers/rules.print.h
@@ -1,13 +1,13 @@
 #ifndef MY_PRINTF_RULLES_PRINT_H
 #define MY_PRINTF_RULLES_PRINT_H
 
-void integerPrint       (void);
-void longPrint          (void);
-void charPrint          (void);
-void stringPrint        (void);
-void pointerPrint       (void);
-void shortPrint         (void);
-void hexadecimalPrint   (void);
+void integerPrint       (var_list ap, char* opt);
+void longPrint          (var_list ap, char* opt);
+void charPrint          (var_list ap, char* opt);
+void stringPrint        (var_list ap, char* opt);
+void pointerPrint       (var_list ap, char* opt);
+void shortPrint         (var_list ap, char* opt);
+void hexadecimalPrint   (var_list ap, char* opt);
 
 void (*print[])(var_list ap, char* opt) = {
    integerPrint,
