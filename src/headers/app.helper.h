18997813f9bd5655a894e5a8b6685e82d1f7be0c
Shikanime <shikanime@hotmail.fr>
2017-03-21 10:30:03 +0100

    deleted:    src/app/process.c 	modified:   src/headers/app.helper.h 	new file:   src/headers/my_printf.h

--- src/headers/app.helper.h
+++ src/headers/app.helper.h
@@ -1,4 +1,9 @@
+#ifndef MY_PRINTF_APP_HELPER_H
+#define MY_PRINTF_APP_HELPER_H
+
 long int    stringLengthHelper(string s);
 void	    charPrintHelper(const char c);
 void        stringPrintHelper(const string s);
 void        integerPrintHelper(int n);
+
+#endif //MY_PRINTF_APP_HELPER
