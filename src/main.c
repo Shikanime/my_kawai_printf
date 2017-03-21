aaa5eb0b41744ad9e7713f8c6c18bf81c6a9580a
Li ZhenHui <phetsi_w@etna-alternance.net>
2017-03-21 07:42:08 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,6 +3,14 @@
 int main(int ac, char **av) {
     (void) ac;
     (void) av;
+    int integerTest;
+    long longTest;
+    char charTest;
+    
+    integerTest = 666;
+    longTest = 3000000000;
+    
+    my_printf("Integer: %d \n Long: %ld \n Char: %c", integerTest, longTest, charTest);
 
     return EXIT_SUCCESS
 }
