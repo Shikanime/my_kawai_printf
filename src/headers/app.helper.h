7fab5d112006367d1bdfc1fbd5f89de517458229
Shikanime <shikanime@hotmail.fr>
2017-03-21 13:05:29 +0100

    modified:   src/headers/app.helper.h 	modified:   src/headers/app.my_printf.h

--- src/headers/app.helper.h
+++ src/headers/app.helper.h
@@ -1,9 +1,9 @@
 #ifndef MY_PRINTF_APP_HELPER_H
 #define MY_PRINTF_APP_HELPER_H
 
-extern size_t    stringLengthHelper(string s);
-extern void	   charPrintHelper(const char c);
-extern void        stringPrintHelper(const string s);
-extern void        integerPrintHelper(int n);
+extern size_t   stringLengthHelper(string s);
+extern void     charPrintHelper(const char c);
+extern void     stringPrintHelper(const string s);
+extern void     integerPrintHelper(int n);
 
 #endif //MY_PRINTF_APP_HELPER
