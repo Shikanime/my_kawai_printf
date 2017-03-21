6f45530ffe7ef643ab430f12f56586356fa49d5a
Shikanime <shikanime@hotmail.fr>
2017-03-21 15:03:39 +0100

    modified:   src/print.c

--- src/print.c
+++ src/print.c
@@ -15,7 +15,9 @@ void (*print[PRINT_NUMBER])(va_list ap, int * opt) = {
 /* Local functions */
 
 void integerPrint(va_list ap, int * opt){
+  stringPrintHelper("alors la?");
   integerPrintHelper(var_arg(ap, int));
+  stringPrintHelper("et la?");
 }
 
 void longPrint(va_list ap, int * opt){
