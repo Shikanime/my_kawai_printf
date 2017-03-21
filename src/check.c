ab3705de186d733bd66241a418dd79a08965e40b
Shikanime <shikanime@hotmail.fr>
2017-03-21 14:52:07 +0100

    modified:   src/check.c 	modified:   src/headers/rules.check.h

--- src/check.c
+++ src/check.c
@@ -14,43 +14,43 @@ int (*check[CHECK_NUMBER])(const char * query, int i) = {
 
 /* local functions */
 
-int integerCheck(const char * query, int n){
+int integerCheck(const char * query, unsigned int n){
   if (query[n] == 'd')
     return 1;
   return 0;
 }
 
-int longCheck(const char * query, int n){
+int longCheck(const char * query, unsigned int n){
   if (query[n] == 'l' && query[n + 1] == 'd')
     return 1;
   return 0;
 }
 
-int charCheck(const char * query, int n){
+int charCheck(const char * query, unsigned int n){
   if (query[n] == 'c' || (query[n]))
     return 1;
   return 0;
 }
 
-int stringCheck(const char * query, int n){
+int stringCheck(const char * query, unsigned int n){
   if (query[n] == 's')
     return 1;
   return 0;
 }
 
-int pointerCheck(const char * query, int n){
+int pointerCheck(const char * query, unsigned int n){
   if (query[n] == 'p')
     return 1;
   return 0;
 }
 
-int shortCheck(const char * query, int n){
+int shortCheck(const char * query, unsigned int n){
   if (query[n] == 'h' && query[n + 1] == 'd')
     return 1;
   return 0;
 }
 
-int hexadecimalCheck(const char * query, int n){
+int hexadecimalCheck(const char * query, unsigned int n){
   if (query[n] == 'x')
     return 1;
   return 0;
--- src/headers/rules.check.h
+++ src/headers/rules.check.h
@@ -3,13 +3,13 @@
 
 #define CHECK_NUMBER 7
 
-int (*check[CHECK_NUMBER])  (const char * query, int i);
-int integerCheck            (const char * query, int n);
-int longCheck               (const char * query, int n);
-int charCheck               (const char * query, int n);
-int stringCheck             (const char * query, int n);
-int pointerCheck            (const char * query, int n);
-int shortCheck              (const char * query, int n);
-int hexadecimalCheck        (const char * query, int n);
+int (*check[CHECK_NUMBER])  (const char * query, unsigned int n);
+int integerCheck            (const char * query, unsigned int n);
+int longCheck               (const char * query, unsigned int n);
+int charCheck               (const char * query, unsigned int n);
+int stringCheck             (const char * query, unsigned int n);
+int pointerCheck            (const char * query, unsigned int n);
+int shortCheck              (const char * query, unsigned int n);
+int hexadecimalCheck        (const char * query, unsigned int n);
 
 #endif //MY_PRINTF_RULLES_CHECK
