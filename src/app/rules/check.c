50028595f7bdc6bd55f0bb3edde091485b3469cd
Shikanime <shikanime@hotmail.fr>
2017-03-21 09:21:26 +0100

    modified:   src/app/rules/check.c

--- src/app/rules/check.c
+++ src/app/rules/check.c
@@ -5,34 +5,41 @@
 bool integerCheck(const string query, int n){
   if (query[n] == 'd')
     return true;
+  return false;
 }
 
 bool longCheck(const string query, int n){
   if (query[n] == 'l' && query[n + 1] == 'd')
     return true;
+  return false;
 }
 
 bool charCheck(const string query, int n){
   if (query[n] == 'c' || (query[n]))
     return true;
+  return false;
 }
 
 bool stringCheck(const string query, int n){
   if (query[n] == 's')
     return true;
+  return false;
 }
 
 bool pointerCheck(const string query, int n){
   if (query[n] == 'p')
     return true;
+  return false;
 }
 
 bool shortCheck(const string query, int n){
   if (query[n] == 'h' && query[n + 1] == 'd')
     return true;
+  return false;
 }
 
 bool hexadecimalCheck(const string query, int n){
   if (query[n] == 'x')
     return true;
+  return false;
 }
