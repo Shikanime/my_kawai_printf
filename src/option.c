d3ec782f09e639eb7f126c65909825429cc12b8b
Shikanime <shikanime@hotmail.fr>
2017-03-22 11:17:48 +0100

    modified:   src/my_printf.c 	modified:   src/option.c

--- src/option.c
+++ src/option.c
@@ -9,9 +9,7 @@ int (*option[OPTION_NUMBER])(const char * query, unsigned int n, int * opt) = {
 /* Local functions */
 
 int longOption(const char * query, unsigned int n, int * opt){
-stringPrintHelper("1");
   if (query[n] == 'h' && query[n + 1] != '\0'){
-stringPrintHelper("2");
       opt[lenghtMod] = 1;
       return 1;
   }
