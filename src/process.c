5c402719e60314d7710906ab82c7b4b6ec79b8f4
Shikanime <shikanime@hotmail.fr>
2017-03-22 13:11:21 +0100

    modified:   src/my_printf.c 	modified:   src/process.c

--- src/process.c
+++ src/process.c
@@ -49,8 +49,7 @@ int charProcess(const char * query, unsigned int n, va_list ap, int * opt){
 int stringProcess(const char * query, unsigned int n, va_list ap, int * opt){
   (void) opt;
   if (query[n] == 's'){
-stringPrintHelper("ici");
-    stringPrintHelper(va_arg(ap, char *));
+    stringPrintHelper(va_arg(ap, void *));
     return 1;
   }
   return 0;
