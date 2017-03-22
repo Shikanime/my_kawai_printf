41178417f7a3a538651e2e312267a56e84b05f40
Shikanime <shikanime@hotmail.fr>
2017-03-22 10:56:33 +0100

    modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -4,11 +4,15 @@ int main(int ac, char * av[]) {
     (void) ac;
     (void) av;
 
+    // Single
     my_printf("Integer: %d", 3);
     my_printf("Escape: %%");
     my_printf("Long: %hd", 444);
     my_printf("Char: %c", 'c');
     my_printf("String: %s", "cest une string");
+
+    // All
+    my_printf("Integer: %d | Escape: %% | Long: %hd | Char: %c | String: %s", 3, 444, 'c', "cest une string");
 
     return 0;
 }
