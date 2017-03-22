ec5174d5163fe1d2a73e337c1955af193a79ff41
Shikanime <shikanime@hotmail.fr>
2017-03-22 09:54:26 +0100

    modified:   src/main.c 	modified:   src/print.c

--- src/print.c
+++ src/print.c
@@ -37,7 +37,7 @@ void charPrint(va_list ap, int * opt){
 
 void stringPrint(va_list ap, int * opt){
   (void) opt;
-  stringPrintHelper(va_arg(ap, char **));
+  stringPrintHelper(va_arg(ap, char *));
 }
 
 void pointerPrint(va_list ap, int * opt){
