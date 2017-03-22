9c56999701e6dc7ae342e823c3f71ed7cdaf9a27
Shikanime <shikanime@hotmail.fr>
2017-03-22 09:57:30 +0100

    modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ int main(int ac, char * av[]) {
     (void) ac;
     (void) av;
 
-    my_printf("Integer: %d \nChar: %c\nString: %s\nEmpty: %\nInteger: %d \n", 666, 3000000000, 'a', "ca marche ici", 23);
+    my_printf("Integer: %d \nChar: %c\nString: %s\nEmpty: %\n", 666, 'a', "ca marche ici");
 
     return 0;
 }
