9b3b380b6661800ae59000c9c35205ec28b5b816
Shikanime <shikanime@hotmail.fr>
2017-03-22 09:15:39 +0100

    modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -6,12 +6,15 @@ int main(int ac, char * av[]) {
 
     int integerTest;
     long longTest;
-    char stringTest[20] = "bite";
+    char charTest;
+    char * stringTest;
 
     integerTest = 666;
     longTest = 3000000000;
+    charTest = 'a';
+    stringTest = "Mec ca marche aussi";
     
-    my_printf("Integer: %d \nLong: %ld \nChar: %s\n Empty: %\n", integerTest, longTest, stringTest);
+    my_printf("Integer: %d \nLong: %ld \nChar: %s\n String: %c\n Empty: %\n", integerTest, longTest, stringTest);
 
     return 0;
 }
