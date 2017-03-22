bd9e3534c9b891e099b9b4beba5e89cdf462bd0e
Shikanime <shikanime@hotmail.fr>
2017-03-22 17:06:03 +0100

    modified:   src/headers/app.helper.h

--- src/headers/app.helper.h
+++ src/headers/app.helper.h
@@ -7,8 +7,8 @@ void        stringPrintHelper               (const char * s);
 void        integerPrintHelper              (int n);
 void        unsignedIntegerPrintHelper      (unsigned int n);
 void	    addressPrintHelper              (unsigned long long n);
-void        HexadecimalPrintHelper          (unsigned long long n);
-void        HexadecimalCapitalPrintHelper   (unsigned long long n);
+void        hexadecimalPrintHelper          (unsigned long long n);
+void        hexadecimalCapitalPrintHelper   (unsigned long long n);
 void	    octalPrintHelper                (unsigned int n);
 
 #endif //MY_PRINTF_APP_HELPER
