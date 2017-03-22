b937b839beafcbaf708edd560926867832e3a535
Shikanime <shikanime@hotmail.fr>
2017-03-22 09:16:13 +0100

    modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@ int main(int ac, char * av[]) {
     charTest = 'a';
     stringTest = "Mec ca marche aussi";
     
-    my_printf("Integer: %d \nLong: %ld \nChar: %s\n String: %c\n Empty: %\n", integerTest, longTest, stringTest);
+    my_printf("Integer: %d \nLong: %ld \nChar: %c\n String: %s\n Empty: %\n", integerTest, longTest, stringTest);
 
     return 0;
 }
