074c54b2ac90845d2ea51493033b51c9add295eb
Shikanime <shikanime@hotmail.fr>
2017-03-22 09:50:07 +0100

    modified:   src/print.c

--- src/print.c
+++ src/print.c
@@ -37,6 +37,7 @@ void charPrint(va_list ap, int * opt){
 
 void stringPrint(va_list ap, int * opt){
   (void) opt;
+  stringPrintHelper("ici ca passe");
   stringPrintHelper(va_arg(ap, char *));
 }
 
