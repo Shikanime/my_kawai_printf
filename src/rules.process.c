91d57e849a1c8cbd1489e319f14b0e960dd2dc4e
Shikanime <shikanime@hotmail.fr>
2017-03-23 00:12:46 +0100

    modified:   src/rules.process.c

--- src/rules.process.c
+++ src/rules.process.c
@@ -99,22 +99,29 @@ int unsignedIntegerProcess(const char * query, int n, int * opt, va_list * ap){
 }
 
 int floatPointProcess(const char * query, int n, int * opt, va_list * ap){
+  (void) query;
   (void) opt;
   if (query[n] == 'f'){
+    va_arg(*ap, float);
     return 1;
   } else if (query[n] == 'F'){
+    va_arg(*ap, float);
     return 1;
   } else if (query[n] == 'G'){
+    va_arg(*ap, float);
     return 1;
   }
   return 0;
 }
 
 int doublePrecisionProcess(const char * query, int n, int * opt, va_list * ap){
+  (void) query;
   (void) opt;
   if (query[n] == 'e'){
+    va_arg(*ap, double);
     return 1;
   } else if (query[n] == 'E'){
+    va_arg(*ap, double);
     return 1;
   }
   return 0;
