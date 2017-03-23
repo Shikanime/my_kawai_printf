fa98ea084adbcfc92f07d617e6504adccf38213e
Li ZhenHui <phetsi_w@etna-alternance.net>
2017-03-23 14:41:26 +0100

    Update and rename src/test.c to main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "headers/loader.h"
+
+int main(int ac, char * av[]) {
+	(void) ac;
+	(void) av;
+
+	my_printf("Default - its a chain\n");
+	my_printf("String - %s\n", "its an another chain");
+	my_printf("Integer - %i\n", 42);
+	my_printf("Mixed - %s %d %s%c", "with", 4, "parametres", '\n');
+	my_printf("Octal - %o\n", 42);
+	my_printf("Unisgned Integer - %u\n", (unsigned int)4200000000);
+	my_printf("Hexadecimal - %x\n", 42);
+	my_printf("Uppercase hexadecimal - %X\n", 42);
+	my_printf("Negative hexadecimal - %x\n", -42);
+	my_printf("Negative uppercase hexadecimal - %X\n", -42);
+	my_printf("Mixed - %d%%\n", -42);
+	
+	return 1;
+}
